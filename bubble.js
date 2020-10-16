b130634174af33857195c3a8c7339b802a1c04fc
aminebaha <aminebaha.info@gmail.com>
2020-10-16 15:15:24 +0200

    bubble updated  again

--- bubble.js
+++ bubble.js
@@ -1,24 +1,25 @@
 module.exports = {
 
     bubble_sort:    (array)=>{
-    let pivot = 0
-    let swap 
-   do {
-    swap = false
-    for (let i = 0; i < array.length; i++) {
-   
-        if(array[i]>array[i+1]) {
-            pivot = array[i]
-            array[i] = array[i+1]
-            array[i+1] = pivot
-            
+        let pivot = 0
+        let swap =  true
+       do {
+        swap = false
+        for (let i = 0; i < array.length; i++) {
+       
+            if(array[i]>array[i+1]) {
+                pivot = array[i]
+                array[i] = array[i+1]
+                array[i+1] = pivot
+                swap =true
+                
+            }
         }
-    }
-}while(swap);
-
-
-    return array
-    }
+    }while(swap);
+     
+        return array
+        }
+    
 
     
 }
